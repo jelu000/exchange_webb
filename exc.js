706f7f8aab55fcb1734e07b3678cf126b9dc7b7c
jelu000 <jelu000@gmail.com>
2023-05-10 14:57:15 +0200

    javascript kvar

---
+++ exc.js
@@ -0,0 +1,6 @@
+const input_pris = document.getElementById("pris");
+const input_inbet = document.getElementById("inbet");
+const svardiv = document.getElementById("svar_div");
+
+const kalkyleraknapp = document.getElementById("kalk_knapp");
+kalkyleraknapp.addEventListener("click", kalkyleraClick);
